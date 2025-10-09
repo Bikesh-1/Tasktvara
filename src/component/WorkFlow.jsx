6a020fb79c72a8e89aeb07c52de10187526d721b
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-10-10 01:28:50 +0530

    Refactor WorkFlow component to enhance layout and responsiveness; remove console.log for production readiness, update image styles, and improve text structure for better readability across devices.

--- src/component/WorkFlow.jsx
+++ src/component/WorkFlow.jsx
@@ -2,17 +2,13 @@ import React from 'react'
 import { ReactLenis, useLenis } from 'lenis/react'
 
 function WorkFlow() {
-    const lenis = useLenis((lenis) => {
-        console.log(lenis)
-    })
     return (
         <div className="flex relative z-10 flex-col gap-4 justify-center items-center w-screen min-h-[130vh] bg-white">
             <ReactLenis root />
-            {/* Responsive flex: column on mobile, row on md+ */}
             <div className="flex flex-col-reverse gap-8 justify-around items-center px-2 w-full sm:w-[70vw] md:flex-row md:gap-14 md:px-8">
                 <div className="flex flex-col items-center w-full md:w-auto md:items-start">
                     <p className="text-2xl xs:text-3xl sm:text-4xl lg:text-5xl text-[#AEAEAE] text-justify p-2 mb-4 md:mb-8">
-                        At Tvara, reliability is built into every <br className="hidden xs:inline" /> 
+                        At Tvara, reliability is built into every <br className="hidden xs:inline" />
                         workflow. Our SNAP architecture <br className="hidden xs:inline" />
                         keeps tasks connected, context <br className="hidden xs:inline" />
                         intact, and processes running the <br className="hidden xs:inline" />
@@ -20,9 +16,9 @@ function WorkFlow() {
                     </p>
                     <a className="text-lg xs:text-xl sm:text-2xl lg:text-4xl underline text-[#1d4f8f] font-semibold" href="#">Get to know how we build workflows</a>
                 </div>
-                <div className="flex justify-center mb-6 w-full md:justify-end md:w-auto md:mb-0">
+                <div className="flex justify-center mb-6 md:justify-end md:mb-0">
                     <img
-                        className="w-full max-w-xs sm:w-[30vw] md:w-[40vw] lg:w-[48vw] xl:w-[60vw] 2xl:w-[70vw] rounded-3xl object-cover"
+                        className="rounded-3xl object-conatin w-[24rem] h-[18rem] sm:w-[100rem] sm:h-[25rem] max-w-full"
                         src="https://ik.imagekit.io/lxvqyrkjo/200fa17ebea45d5430ad1a846a875fdc17685566.png?updatedAt=1759775706103"
                         alt=""
                     />
